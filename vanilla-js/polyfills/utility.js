e5ae7aa09ec277268e38628d6148d3959a5641a1
Shivam <shivam89raj45@gmail.com>
2026-09-02 17:06:07 +0530

    Add utitlity methods

---
+++ vanilla-js/polyfills/utility.js
@@ -0,0 +1,28 @@
+// 1. Basic debounce
+function debounce(cb, delay = 500) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => cb.apply(this, args), delay);
+    }
+}
+
+// 2. Basic throttle
+function throttle(cb, delay = 500) {
+    let lastExecuted = 0;
+    return function(...args) {
+        const now = Date.now();
+        if (now - lastExecuted < delay) return;
+        lastExecuted = now;
+        cb.apply(this, args);
+    }
+}
+
+function throttleV2(cb, delay = 500) {
+    let timer;
+    return function(...args) {
+        if (timer) return;
+        cb.apply(this, args);
+        timer = setTimeout(() => timer = undefined, delay);
+    }
+}
